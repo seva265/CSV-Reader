496bba2fe4c4ccf3d437ba2e4aa8a1fe4ff3ab38
Seva P <seva@parakhin.name>
2026-03-21 19:13:40 +0300

    feature: header validation

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+import csv
+import codecs
+from io import TextIOWrapper
+
+from fastapi import FastAPI, File, UploadFile, HTTPException
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/upload-grades")
+async def upload_grades(file: UploadFile = File(...)):
+    if not file.filename:
+        raise HTTPException(status_code=400, detail='File has no name')
+    if not file.filename.lower().endswith('.csv'):
+        raise HTTPException(status_code=400, detail='Not a .csv')
+    stream = codecs.iterdecode(file.file, 'utf-8')
+    csv_reader = csv.DictReader(stream)
+
+    expected_headers = ['Дата', 'Номер группы', 'ФИО', 'Оценка']
+    if csv_reader.fieldnames != expected_headers:
+        raise HTTPException(status_code=400, detail='Invalid headers')
+    
+    
